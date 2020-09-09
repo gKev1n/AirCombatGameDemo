688df16a170a14df45606441f506c2910b846fe8
gKev1n <39189948+gKev1n@users.noreply.github.com>
2020-09-09 22:46:36 +0800

    202009091046 Initial

---
+++ AirCombatGame/Assets/Editor/TextureSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TextureSetting : AssetPostprocessor
+{
+   public void OnPreprocessTexture()
+   {
+      TextureImporter importer = (TextureImporter) assetImporter;
+      importer.textureType = TextureImporterType.Sprite;
+   }
+}
